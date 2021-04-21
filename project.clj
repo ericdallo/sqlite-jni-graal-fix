474e83eae89c417cfa70c5d4913d0ed35ddf9acd
Eric Dallo <ericdallo06@hotmail.com>
2021-04-20 21:55:43 -0300

    Bump graal version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ericdallo/sqlite-jni-graal-fix "0.0.3-graalvm-21.0.0"
+(defproject ericdallo/sqlite-jni-graal-fix "0.0.3-graalvm-21.1.0"
   :description "A fix for sqlite issue with jni missing classes in GraalVM native-image."
   :license {:name "Eclipse Public License 1.0"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
@@ -7,7 +7,7 @@
         :url "https://github.com/ericdallo/sqlite-jni-graal-fix"}
   :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.xerial/sqlite-jdbc "3.34.0"]
-                 [org.graalvm.nativeimage/svm "21.0.0"]]
+                 [org.graalvm.nativeimage/svm "21.1.0"]]
   :java-source-paths ["src-java"]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
